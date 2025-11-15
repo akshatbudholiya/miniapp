e354288e72cc84acfed5bea5e42da2ee11566680
Akshat Narayan Budholiya <akshatnarayanb@gmail.com>
2025-11-15 15:05:57 +0530

    Updated files

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login.jsx";
-import Terms from "./pages/Terms.jsx";
-import Pricelist from "./pages/Pricelist.jsx";
-import PricelistPage from "./pages/Pricelist.css";
+import Login from "./Login.jsx";
+import Terms from "./Terms.jsx";
+import Pricelist from "./Pricelist.jsx";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/pricelist" element={<Pricelist />} />
-        <Route path="/pricelist" element={<PricelistPage />} />
       </Routes>
     </Router>
   );
