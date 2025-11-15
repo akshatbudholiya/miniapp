aca98a08d0295d275bc077bb1b1788830df79556
Akshat Narayan Budholiya <akshatnarayanb@gmail.com>
2025-11-15 15:24:34 +0530

    Updated vite

--- frontend/Login.jsx
+++ frontend/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../lib/supabaseClient.js";
+import { supabase } from "./src/lib/supabaseClient.js";
 import "./Login.css";
 
 function Login() {
