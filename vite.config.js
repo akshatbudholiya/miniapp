ce8517a44d6e1912a4438b109fe9db4075f6768d
Akshat Narayan Budholiya <akshatnarayanb@gmail.com>
2025-11-15 14:49:05 +0530

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'Frontend',
+  root: 'frontend',
   plugins: [react()],
 
   build: {
